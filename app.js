34ee6ea2030f319908c70e020fa5012850d04a4d
Servet <83537967+servettonga@users.noreply.github.com>
2022-12-08 20:55:55 +0100

    Updates or deletes notes with their attachments

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
-import { User, File, Note, connectDatabase } from './database.js';
+import { connectDatabase } from './database.js';
+import { User, File, Note } from './models.js';
+import { verifyToken, createCookies, extendCookies, clearCookies } from './functions.js';
 import express from 'express';
-import bodyParser from 'body-parser';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 import multer from 'multer';
+import fs from 'fs';
+import bytes from 'bytes';
+
+
+// app settings ----------------------------------------------------------------
 
 // Environment variables
 dotenv.config();
@@ -19,74 +25,16 @@ const upload = multer({ dest: './public/data/uploads' })
 // Express settings
 const port = process.env.API_PORT
 const app = express();
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
 
-// Token verification
-const verifyToken = (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.cookies["x-access-token"];
+// Routes ----------------------------------------------------------------
 
-    if (!token) {
-        return res.status(403).redirect('/login');
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-        req.user = decoded;
-    } catch (err) {
-        return res.status(401).redirect('/login')
-    }
-
-    return next();
-};
-
-
-// Routes
-
-app.route("/notes")
-
-    .get(verifyToken, async (req, res) => {
-        const user = await User.findById({ _id: req.cookies.userid })
-        const username = user.name
-        const notes = await Note.find({ owner: user }).populate("attachment")
-        res.status(200).render("notes", { username, notes })
-    })
-
-    .post(upload.single('noteAttachment'), async (req, res) => {
-        const owner = req.cookies.userid
-        const title = req.body.noteTitle
-        const body = req.body.noteBody
-
-        async function upload() {
-            if (req.file !== undefined && req.file.size < 5000000) {
-                const attachmentData = {
-                    path: req.file.path,
-                    fileName: req.file.originalname,
-                    fileSize: `${(req.file.size * 0.000009765625).toFixed(2)} MB`,
-                    owner
-                }
-                const file = await File.create(attachmentData)
-                file.save()
-                return file.id
-            } return null
-        };
-
-        const noteToSave = new Note({ title, body, attachment: await upload(), owner });
-        noteToSave.save();
-
-        res.redirect("/notes");
-    })
-
-
-app.get("/download/:file", async (req, res) => {
-    const file = await File.findById({ _id: req.params.file })
-    res.download(`${file.path}`, file.fileName)
-})
-
+// Registration
 app.route("/register")
 
     .post(async (req, res) => {
@@ -126,7 +74,7 @@ app.route("/register")
             user.token = token;
 
             // return new user
-            cookies(res, user)
+            createCookies(res, user)
 
         } catch (err) {
             console.log(err);
@@ -134,9 +82,81 @@ app.route("/register")
     });
 
 
+// Notes
+app.all('/notes', async (req, res, next) => {
+    await verifyToken(req, res);
+    next();
+})
+
+app.route("/notes")
+
+    .get(async (req, res) => {
+        extendCookies(req, res);
+        const user = await User.findOne({ email: req.cookies.username })
+        const username = user.name
+        const notes = await Note.find({ owner: user }).populate('attachment')
+        res.status(200).render("notes", { username, notes })
+    })
+
+    .post(upload.single('noteAttachment'), async (req, res) => {
+        const owner = await User.findOne({ email: req.cookies.username })
+        const title = req.body.noteTitle
+        const body = req.body.noteBody
+
+        async function upload() {
+            if (req.file !== undefined && req.file.size < 5000000) {
+                const attachmentData = {
+                    path: req.file.path,
+                    fileName: req.file.originalname,
+                    fileSize: bytes.format(req.file.size, { unitSeparator: ' ' }),
+                    owner
+                }
+                const file = await File.create(attachmentData)
+                file.save()
+                return file.id
+            } return null
+        };
+
+        const noteToSave = new Note({ title, body, attachment: await upload(), owner });
+        noteToSave.save();
+
+        res.redirect("/notes");
+    })
+
+
+// Download attachment
+app.get("/notes/download/:file", async (req, res) => {
+    const file = await File.findById({ _id: req.params.file })
+    res.download(`${file.path}`, file.fileName)
+});
+
+
+// Update or delete items
+app.post("/notes/update/:id", async (req, res) => {
+    const id = req.params.id;
+    const done = (req.body.done)
+    const button = (req.body.button)
+
+    if (button) {
+        const note = await Note.findById({ _id: id })
+        const attachment = await File.findById({ _id: note.attachment._id })
+        fs.unlink(`./${attachment.path}`, err => { if (err) console.log(err) })
+        await File.deleteOne(attachment);
+        await Note.deleteOne(note);
+
+    }
+    if (done) {
+        await Note.updateOne({ _id: id }, { done });
+    }
+
+    res.redirect("/notes");
+});
+
+
+// Login user
 app.route("/login")
 
-    .get((req, res) => {
+    .get(async (req, res) => {
         if (req.cookies.loggedin) {
             res.redirect('/notes')
         } else {
@@ -171,12 +191,12 @@ app.route("/login")
                 existingUser.token = token;
 
                 // Redirect
-                cookies(res, existingUser)
+                createCookies(res, existingUser)
 
             } else {
                 res
                     .status(400)
-                    .render("login", { warning: true, username: '' })
+                    .render("login", { warning: "Invalid username or password", username: '' })
             }
 
         } catch (err) {
@@ -185,44 +205,19 @@ app.route("/login")
     });
 
 
+// Logout user and clear cookies
 app.route("/logout")
 
     .get((req, res) => {
-
-        // Logout and clear cookies 
         clearCookies(res)
     });
 
 
+// 404
 app.use("*", (req, res) => {
     res.redirect('/login');
 });
 
 
-// Create cookies and redirect
-function cookies(res, existingUser) {
-    const expire = new Date(Date.now() + 1 * 3600000) // cookie will be removed after 1 hour
-    return res
-        .status(201)
-        .cookie('x-access-token', existingUser.token, {
-            expires: expire
-        })
-        .cookie('loggedin', true, {
-            expires: expire
-        })
-        .cookie('userid', existingUser._id, {
-            expires: expire
-        })
-        .cookie('username', existingUser.email)
-        .redirect(301, '/notes')
-};
-
-function clearCookies(res) {
-    res
-        .clearCookie('x-access-token', 'loggedin', 'userid')
-        .clearCookie('loggedin')
-        .clearCookie('userid')
-        .redirect('/login')
-};
-
+// Run server ----------------------------------------------------------------
 app.listen(port, () => { console.log(`Server running on port ${port}`) });
--- database.js
+++ database.js
@@ -1,6 +1,5 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { format } from 'date-fns';
 
 dotenv.config();
 
@@ -18,68 +17,3 @@ export function connectDatabase() {
             process.exit(1);
         });
 };
-
-const userSchema = new Schema({
-    name: {
-        type: String,
-        unique: false,
-        required: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-    },
-    token: {
-        type: String
-    }
-});
-
-const fileSchema = new Schema({
-    fileName: {
-        type: String,
-        required: true
-    },
-    path: {
-        type: String,
-        required: true
-    },
-    fileSize: String,
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
-});
-
-const noteSchema = new Schema({
-    done: {
-        type: Boolean,
-        default: false
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    body: String,
-    date: {
-        type: String,
-        default: format(new Date(), 'dd MMMM yyyy')
-    },
-    attachment: {
-        type: Schema.Types.ObjectId,
-        ref: 'File'
-    },
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
-});
-
-export const Note = mongoose.model("Note", noteSchema);
-
-export const File = mongoose.model("File", fileSchema);
-
-export const User = mongoose.model("User", userSchema);
